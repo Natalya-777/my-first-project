0e2a0ef9a1b3a8ff7ba421f8b09be71c2f4ee59b
Наталья-777 <valdevin@yandex.ru>
2025-10-02 12:59:56 +0300

    Добавлен новый проект

---
+++ My-first-prodject/My-first-prodject/My-first-prodject.cpp
@@ -0,0 +1,15 @@
+﻿
+
+#include <iostream>
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+
+    std::cout << "Список дел: \n";
+    std::cout << "Бег и тренировка на тренажерах\n";
+    std::cout << "Сходить в магазин\n";
+    std::cout << "Ответить подруге\n";
+    std::cout << "Сделать ДЗ ТОП академия\n";
+    std::cout << "Сходить на учебу\n";
+}
