2e77b5df93df0d42b4d751de73492235cc845bef
Наталья-777 <valdevin@yandex.ru>
2025-10-02 14:43:45 +0300

    Добавлена функция сохранения задач в файл

--- My-first-prodject/My-first-prodject/My-first-prodject.cpp
+++ My-first-prodject/My-first-prodject/My-first-prodject.cpp
@@ -1,15 +1,47 @@
 ﻿
 
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+
+void saveTasksToFile(const std::vector<std::string>& tasks, const std::string& filename) {
+    std::ofstream file(filename);
+    if (file.is_open()) {
+        for (const auto& task : tasks) {
+            file << task << std::endl;
+        }
+        file.close();
+        std::cout << "Задачи успешно сохранены в файл " << filename << std::endl;
+    }
+    else {
+        std::cerr << "Не удалось открыть файл для записи." << std::endl;
+    }
+}
 
 int main()
 {
     setlocale(LC_ALL, "ru");
 
+    
+    std::vector<std::string> tasks = {
+        "Бег и тренировка на тренажерах",
+        "Ответить подруге",
+        "Сделать ДЗ ТОП академия",
+        "Сходить на учебу",
+        "Встреча с подругой"
+    };
+
+   
     std::cout << "Список дел: \n";
-    std::cout << "Бег и тренировка на тренажерах\n";  
-    std::cout << "Ответить подруге\n";
-    std::cout << "Сделать ДЗ ТОП академия\n";
-    std::cout << "Сходить на учебу\n";
-    std::cout << "Встреча с подругой";
+    for (const auto& task : tasks) {
+        std::cout << task << std::endl;
+    }
+
+    
+    std::string filename = "tasks.txt"; 
+    saveTasksToFile(tasks, filename);
+
+    return 0;
 }
+
